8819a8297d8ff2d5c98f760275670be2404eacd8
Deluan Quintao hg <Deluan Quintao hg@deluan.com.br>
2010-09-23 19:48:14 -0300

    checked if users were already created

--- src/main/webapp/WEB-INF/groovy/createUsers.groovy
+++ src/main/webapp/WEB-INF/groovy/createUsers.groovy
@@ -1,5 +1,6 @@
-import com.google.appengine.api.datastore.*
 import com.deluan.shiro.gae.realm.DatastoreRealm
+import com.google.appengine.api.datastore.Entity
+import com.google.appengine.api.datastore.Query
 import org.apache.shiro.crypto.hash.Sha512Hash
 
 def saveUser(username, password) {
@@ -12,10 +13,23 @@ def saveUser(username, password) {
     return entity
 }
 
+def hasUsers() {
+    def query = new Query(DatastoreRealm.DEFAULT_USER_STORE_KIND)
 
-saveUser("admin", "admin")
-saveUser("user1", "pass1")
 
-println "Created Users:<br/><br/>"
+    def result = datastore.prepare(query).countEntities() > 0
+}
+
+
+if (!hasUsers()) {
+    saveUser("admin", "admin")
+    saveUser("user1", "pass1")
+    println "Created some users:<br/><br/>"
+} else {
+    println "Users were already created:<br/><br/>"
+}
+
 println "admin / admin<br/>"
 println "user1 / pass1<br/>"
+
+println "<br/><a href=\"/\">back</a>"
